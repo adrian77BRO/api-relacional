022915c222490a470e8c9bd446e1d02272ea7636
Lalo64GG <223257@ids.upchiapas.edu.mx>
2023-11-03 11:20:36 -0600

    Seeder creado

---
+++ src/seeders/vocabulario.seeders.js
@@ -0,0 +1,37 @@
+const conexion = require('../db/database');
+
+// Seed data
+const librosSeedData = [
+    {
+        titulo: 'Libro 1',
+        descripcion: 'Descripción del Libro 1',
+        contenido: 'Contenido del Libro 1'
+    },
+    {
+        titulo: 'Libro 2',
+        descripcion: 'Descripción del Libro 2',
+        contenido: 'Contenido del Libro 2'
+    },
+    {
+        titulo: 'Libro 3',
+        descripcion: 'Descripción del Libro 3',
+        contenido: 'Contenido del Libro 3'
+    }
+];
+
+// Seeder function
+const seedLibros = () => {
+    librosSeedData.forEach(libro => {
+        conexion.query('INSERT INTO libro_vocabulario (titulo, descripcion, contenido) VALUES (?, ?, ?)',
+            [libro.titulo, libro.descripcion, libro.contenido], (error) => {
+                if (error) {
+                    console.error('Error al insertar datos de libro:', error.message);
+                } else {
+                    console.log('Libro insertado exitosamente');
+                }
+            });
+    });
+}
+
+// Ejecutar el seeder
+seedLibros();
